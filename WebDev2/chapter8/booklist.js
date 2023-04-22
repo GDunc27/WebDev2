1bbdabf38a3e80aa46323f280afb676f7be50a36
GDunc27 <122570167+GDunc27@users.noreply.github.com>
2023-04-21 21:32:04 -0500

    book list chapter 8

--- WebDev2/chapter8/booklist.js
+++ WebDev2/chapter8/booklist.js
@@ -1,1 +1,18 @@
 "use strict";
+
+$(document).ready(function() {
+    $("#categories h2").click( evt => {
+        const h2 = evt.currentTarget;
+
+        $(h2).toggleClass("minus");
+
+        if ($(h2).attr("class") !== "minus") {
+            $(h2).next().hide();
+        }
+        else {
+            $(h2).next().show();
+        }
+
+        $("#image").attr("src", "");
+    }); 
+});
